cd178ee9060d74f9b9ba5ae2507746490d890429
Greenwicher <weizhiliu2009@gmail.com>
2016-12-24 20:47:02 +0800

    add project for Financial Systems Simulation

---
+++ NJU 金融系统仿真/README.md
@@ -0,0 +1,5 @@
+#Info
+
+> This directory contains all the souce code, and documentation files for the project I have done in the course "Financial Systems Simulation" when I'm in Nanjing University.
+
+In particular, our project topic is "Credit Scoring using Random Forests." Several machine learning algorithms including the logistic regression, classification tree methods, and random forests are implemented based on R language to score the credit of the borrower. With the help of credit scoring, the business bank can alleviate the possibility of bad debt. 
---
+++ NJU 金融系统仿真/scripts/CreditScoring.R
@@ -0,0 +1,143 @@
+setwd("e:/tmp/fss_h/data")
+getwd()
+set.seed(2013)
+
+library(RSADBE)
+library(ROCR)
+library(randomForest)
+library(rpart)
+
+## get data from the RSADBE package
+data(GC)
+data<-GC
+data$purpose<-as.integer(data$purpose)
+data$property<-as.integer(data$property)
+
+## seperate the data set into two domains, train and test set
+train.rate=0.6
+d=sort(sample(nrow(data), nrow(data)*train.rate))
+train=data[d,]
+test=data[-d,]
+
+## logistic regression
+fit1<-glm(good_bad~., data=train, family=binomial(link="logit"))
+summary(fit1)
+write.csv(coef(summary(fit1)),  'logistic.csv')
+test$score1<-predict(fit1, type='response', test)
+pred1<-prediction(test$score1, test$good_bad)
+perf1<-performance(pred1, 'tpr', 'fpr')
+
+# top 3 reasons
+g<-predict(fit1,type='terms',test)  
+ftopk<- function(x,top=3){  
+  
+  res=names(x)[order(x, decreasing = TRUE)][1:top] 
+  paste(res,collapse=";",sep="") 
+} 
+# Application of the function using the top 3 rows 
+topk=apply(g,1,ftopk,top=3) 
+
+
+## simple Classficatioin Tree
+fit2<-rpart(good_bad~.,data=train) 
+plot(fit2, main='Classfication Tree');text(fit2); 
+test$score2<-predict(fit2,type='prob',test)[,2] 
+pred2<-prediction(test$score2,test$good_bad) 
+perf2<-performance(pred2,"tpr","fpr") 
+
+# generate rules
+listrules<-function(model) 
+{ 
+  
+  if (!inherits(model, "rpart")) stop("Not a legitimate rpart tree") 
+  # 
+  # Get some information. 
+  # 
+  frm<- model$frame 
+  names<-row.names(frm) 
+  ylevels<-attr(model, "ylevels") 
+  ds.size<-model$frame[1,]$n 
+  # 
+  # Print each leaf node as a rule. 
+  # 
+  for(i in 1:nrow(frm)) 
+  { 
+    if (frm[i,1] == "<leaf>" & ylevels[frm[i,]$yval]=='bad') 
+    { 
+      # The following [,5] is hardwired - needs work! 
+      cat("\n") 
+      cat(sprintf(" Rule number: %s ", names[i])) 
+      cat(sprintf("[yval=%s cover=%d N=%s Y=%s (%.0f%%) prob=%0.2f]\n", 
+                  ylevels[frm[i,]$yval], frm[i,]$n,  
+                  formatC(frm[i,]$yval2[,2], format = "f", digits = 2), 
+                  formatC(frm[i,]$n-frm[i,]$yval2[,2], format = "f", digits  
+                          = 2), 
+                  round(100*frm[i,]$n/ds.size), frm[i,] 
+                  $yval2[,5])) 
+      pth <- path.rpart(model, nodes=as.numeric(names[i]),  
+                        print.it=FALSE) 
+      cat(sprintf("   %s\n", unlist(pth)[-1]), sep="") 
+    } 
+  } 
+} 
+
+listrules(fit2) 
+
+
+## random forests
+fit3<-randomForest(good_bad~., data=train, importance=TRUE, proximity=TRUE, ntree=500, keep.forest=TRUE)
+# importance variables
+varImpPlot(fit3, main='Variable Importance measured by Random Forests')
+plot(fit3, main='Error Rates of Random Forests')
+test$score3<-predict(fit3,test,type='prob')[,2] 
+pred3<-prediction(test$score3,test$good_bad) 
+perf3<-performance(pred3,"tpr","fpr") 
+
+# top 3 reasons
+# add reason list to scored tets sample 
+test<-cbind(test, topk)
+
+
+## performance evaluation
+# ROC (Receiver Operating Characteristic) Curve
+plot(perf1, col='red', main='Logistic Regression, Classfication Tree, Random Forests')
+plot(perf2, col='green', add=TRUE)
+plot(perf3, col='blue', add=TRUE)
+legend(0.8,0.6,c('LR','CT','RF'),col=c('red','green','blue'),lwd=3) 
+
+# KS statistic, mostly used in industry
+ks1<-max(attr(perf1,'y.values')[[1]]-attr(perf1,'x.values')[[1]]) 
+ks2<-max(attr(perf2,'y.values')[[1]]-attr(perf2,'x.values')[[1]]) 
+ks3<-max(attr(perf3,'y.values')[[1]]-attr(perf3,'x.values')[[1]]) 
+ks<-c(ks1, ks2, ks3)
+
+# Area under ROC Curve
+auc1<-attr(performance(pred1, 'auc'), 'y.values')[[1]]
+auc2<-attr(performance(pred2, 'auc'), 'y.values')[[1]]
+auc3<-attr(performance(pred3, 'auc'), 'y.values')[[1]]
+auc<-c(auc1, auc2, auc3)
+
+# accuracy rate
+acc1<-max(attr(performance(pred1, 'acc'), 'y.values')[[1]])
+acc2<-max(attr(performance(pred2, 'acc'), 'y.values')[[1]])
+acc3<-max(attr(performance(pred3, 'acc'), 'y.values')[[1]])
+acc<-c(acc1, acc2, acc3)
+
+# optimal cutoff point
+list1<-unlist(attr(performance(pred1, 'acc'), 'x.values')[[1]])
+list2<-unlist(attr(performance(pred2, 'acc'), 'x.values')[[1]])
+list3<-unlist(attr(performance(pred3, 'acc'), 'x.values')[[1]])
+cutoff1<-list1[which.max(attr(performance(pred1, 'acc'), 'y.values')[[1]])]
+cutoff2<-list2[which.max(attr(performance(pred2, 'acc'), 'y.values')[[1]])]
+cutoff3<-list3[which.max(attr(performance(pred3, 'acc'), 'y.values')[[1]])]
+cutoff<-c(cutoff1, cutoff2, cutoff3)
+
+colnames<-c('Model', 'KS statistic', 'Area under the Curve', 'Accuracy Rate', 'Optimal Cutoff')
+model<-c('Logistic Regression', 'Classfication Tree', 'Random Forests')
+stat<-data.frame(model, ks, auc, acc, cutoff)
+colnames(stat)=colnames
+write.csv(stat, 'stat.csv')
+
+write.csv(test, 'test.csv')
+
+save.image('CreditScoring.RData')
